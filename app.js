5293e4861614f360f10a5c5988a30ca8c830ad4a
Byeong Min Cho <merrybmc@gmail.com>
2024-05-20 01:24:24 +0900

    create 데이터 추가

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-// const { MongoClient } = require('mongodb');
-
-// 이것도 될까?
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 // Mongodb 주소
-const uri = 'mondodb://localhost:27017';
+const uri = 'mongodb://localhost:27017';
 
 // uri 정보가 연결되어있는 클라이언트
 const client = new MongoClient(uri);
 
-function run() {
+async function run() {
   // DB 생성
-  const database = client.database('firstDB2');
+  const database = client.db('firstDB2');
   // DB의 Collection 생성
-  const collection = database.collection('users2');
+  const users = database.collection('users2');
+
+  // create 데이터 추가
+  //   const userData = await users.insertOne({ name: 'bmc', age: 30 });
+  //   console.log('result', userData);
+
+  const userList = [
+    { name: 'bmc', age: 30 },
+    { name: 'jessica', age: 25 },
+  ];
+
+  const userListCreate = await users.insertMany(userList);
+  console.log(userListCreate);
 }
+
+run();
