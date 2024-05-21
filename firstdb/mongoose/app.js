225adc800445463b07feb6ce1033ad32d5868e8c
Byeong Min Cho <merrybmc@gmail.com>
2024-05-22 07:19:05 +0900

    validate 세팅 및 read

--- firstdb/mongoose/app.js
+++ firstdb/mongoose/app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // mongoose-test -> DB 이름
 mongoose.connect('mongodb://localhost:27017/mongoose-test');
-
+const validator = require('validator');
 const { Schema } = mongoose;
 
 // 회원 정보 Schema 구조
@@ -11,12 +11,27 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
+    required: true, // 필수값
+    // email validate
+    validate: {
+      validator: (value) => {
+        if (validator.isEmail(value)) {
+          throw new Error('This is not an email');
+        }
+        // if (!value.includes('@')) {
+        //   throw new Error('This is not an email');
+        // }
+      },
+    },
   },
   password: {
     type: String,
+    required: true, // 필수값
+    trim: true, // 공백 제거
   },
   age: {
     type: Number,
+    default: 0, // 기본값 0
   },
 });
 
@@ -25,5 +40,8 @@ const userSchema = new Schema({
 const User = mongoose.model('User', userSchema);
 
 // 값 저장 create
-const newUser = new User({ name: 'bmc', email: '123@gmail.com', password: 12345, age: 30 });
+const newUser = new User({ name: 'bmc', email: '1234@gmail.com', password: 12345, age: 30 });
 newUser.save().then((value) => console.log('value is ', value));
+
+// 값 불러오기 read
+User.find().then((value) => console.log('data', value));
